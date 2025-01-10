3dab054e438c8e6844be3ffc85e8289b314e12bd
wmvieira21 <williammartinsvieira334@gmail.com>
2025-01-10 12:54:09 -0300

    quiz buttons

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -4,25 +4,29 @@ class AnswerButton extends StatelessWidget {
   const AnswerButton({
     super.key,
     required this.text,
-    required this.answer,
+    required this.onTap,
   });
 
   final String text;
-  final Function() answer;
+  final Function() onTap;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
-        backgroundColor: const Color.fromARGB(255, 63, 7, 128),
-        foregroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(40),
-        ),
-        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+          backgroundColor: const Color.fromARGB(255, 63, 7, 128),
+          foregroundColor: Colors.white,
+          //fixedSize: Size.fromWidth(300),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10)),
+      onPressed: onTap,
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 16),
+        textAlign: TextAlign.center,
       ),
-      onPressed: answer,
-      child: Text(text),
     );
   }
 }
--- lib/questions_quiz_screen.dart
+++ lib/questions_quiz_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsQuizScreen extends StatefulWidget {
   const QuestionsQuizScreen({super.key});
@@ -10,27 +12,45 @@ class QuestionsQuizScreen extends StatefulWidget {
   }
 }
 
-void nextQuestion() {}
-
 class _QuestionsQuizScreen extends State<QuestionsQuizScreen> {
+  int questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[questionIndex];
+
     return SizedBox(
       //centralizar verticalmente
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'question...',
-            style: TextStyle(color: Colors.white, fontSize: 30),
-          ),
-          SizedBox(height: 30),
-          AnswerButton(text: 'text', answer: nextQuestion),
-          AnswerButton(text: 'text', answer: nextQuestion),
-          AnswerButton(text: 'text', answer: nextQuestion),
-          AnswerButton(text: 'text', answer: nextQuestion),
-        ],
+      child: Container(
+        margin: EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: GoogleFonts.lato(
+                textStyle: Theme.of(context).textTheme.displayLarge,
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.w700,
+                fontStyle: FontStyle.italic,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(text: answer, onTap: answerQuestion);
+            }),
+          ],
+        ),
       ),
     );
   }
